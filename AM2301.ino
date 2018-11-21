807eae8e3be11109894cc1139e49639847fb01ff
jukkaadler <39553264+jukkaadler@users.noreply.github.com>
2018-11-21 13:50:48 +0200

    Add files via upload

---
+++ AM2301.ino
@@ -0,0 +1,38 @@
+
+//Kirjastot
+#include <dht.h>
+dht DHT;
+
+
+//Vakiot
+#define DHT21 2     // DHT21  (AM2302) - Missä pinnissä ollaan
+
+
+//Muuttujat
+float hum;  //Tallentaa kosteuden arvon
+float temp; //Tallentaa lämpötilan arvon
+
+
+void setup()
+
+
+{
+    Serial.begin(9600);
+}
+
+void loop()
+
+
+{
+    int chk = DHT.read21(DHT21);
+    //Lukee datan ja tallentaa sen muuttujiin hum ja temp    
+    hum = DHT.humidity;
+    temp= DHT.temperature;
+    //Kirjoittaa kosteuden ja lämpötilan arvot sarjaporttiin
+    Serial.print("Kosteus: ");
+    Serial.print(hum);
+    Serial.print(" %, Lämpötila: ");
+    Serial.print(temp);
+    Serial.println(" °C");
+    delay(2000); //Kahden sekunnin viive
+}
